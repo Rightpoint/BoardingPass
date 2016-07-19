47d4413a1e82abde893c026a3196c6239c4c876b
Michael Skiba <mike.skiba@raizlabs.com>
2016-07-19 16:30:24 -0400

    initialized xcode project

---
+++ BoardingPass/BoardingPass.h
@@ -0,0 +1,19 @@
+//
+//  BoardingPass.h
+//  BoardingPass
+//
+//  Created by Michael Skiba on 7/19/16.
+//  Copyright © 2016 Raizlabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BoardingPass.
+FOUNDATION_EXPORT double BoardingPassVersionNumber;
+
+//! Project version string for BoardingPass.
+FOUNDATION_EXPORT const unsigned char BoardingPassVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BoardingPass/PublicHeader.h>
+
+
