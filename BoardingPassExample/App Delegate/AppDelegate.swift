7a7593ab153891b50831c7c0e080400f8e003898
Derek Ostrander <djostran@gmail.com>
2016-11-09 11:36:36 -0800

    Swift3.0 - Remove cross-compatibility in example

--- BoardingPassExample/App Delegate/AppDelegate.swift
+++ BoardingPassExample/App Delegate/AppDelegate.swift
@@ -13,8 +13,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    #if swift(>=3.0)
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         window = UIWindow(frame: UIScreen.main.bounds)
@@ -24,18 +22,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.tintColor = UIColor.red
         return true
     }
-
-    #else
-
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
-        window = UIWindow(frame: UIScreen.mainScreen().bounds)
-
-        window?.rootViewController = OnboardingWrapperViewController.sampleOnboarding()
-        window?.makeKeyAndVisible()
-        window?.tintColor = UIColor.red
-        return true
-    }
-
-    #endif
 }
